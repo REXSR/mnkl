98e990252846b8775a7c379f38f28c6659974210
REXSR <41483292+REXSR@users.noreply.github.com>
2018-11-16 21:19:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@ setTimeout(() => mohamed.send(`**Welcome to Power Server..**`), 4000)
 
 const prefix = "$"
 
-
+const adminprefix = "#"; 
 const devs = ['232564113459511306']; 
 client.on('message', message => { 
   var argresult = message.content.split(` `).slice(1).join(' '); 
@@ -49,49 +49,10 @@ if (message.content.startsWith(adminprefix + 'setT')) {
 
 
 
-client.on('message', message => {
 
-  if (!message.content.startsWith(prefix)) return;
 
-  var args = message.content.split(' ').slice(1);
 
-  var argresult = args.join(' ');
 
-  if (message.author.id !== '232564113459511306') return;
-
-if (message.content.startsWith(prefix + 'g')) {
-
-  client.user.setGame(argresult);
-
-    message.channel.sendMessage(white_check_mark  : ${argresult})
-
-} else 
-
-if (message.content.startsWith(prefix + 'w')) {
-
-client.user.setActivity(argresult, {type:'WATCHING'});
-
-    message.channel.sendMessage(white_check_mark  : ${argresult})
-
-} else 
-
-if (message.content.startsWith(prefix + 'l')) {
-
-client.user.setActivity(argresult, {type:'LISTENING'});
-
-    message.channel.sendMessage(white_check_mark  : ${argresult})
-
-} else 
-
-if (message.content.startsWith(prefix + 's')) {
-
-  client.user.setGame(argresult, "https://www.twitch.tv/ht3998425);
-
-    message.channel.sendMessage(white_check_mark  : ${argresult})
-
-}
-
-});
 
 
 
