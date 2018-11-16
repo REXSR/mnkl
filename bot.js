d6a9a9aca6777c3e85a3a8720bf5e75621fb84dc
REXSR <41483292+REXSR@users.noreply.github.com>
2018-11-16 21:07:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -22,6 +22,33 @@ setTimeout(() => mohamed.send(`**Welcome to Power Server..**`), 4000)
 const prefix = "$"
 
 
+const devs = ['232564113459511306']; 
+client.on('message', message => { 
+  var argresult = message.content.split(` `).slice(1).join(' '); 
+    if (!devs.includes(message.author.id)) return; 
+    
+if (message.content.startsWith(adminprefix + 'setgame')) { 
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**${argresult} تم تغيير بلاينق البوت إلى **`)
+} else 
+  if (message.content.startsWith(adminprefix + 'setname')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(`**${argresult}** : تم تغيير أسم البوت إلى`)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setavatar')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(`**${argresult}** : تم تغير صورة البوت`);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk");
+    message.channel.sendMessage(`**تم تغيير تويتش البوت إلى  ${argresult}**`)
+}
+
+});
+
+
+
 client.on('message', message => {
 
   if (!message.content.startsWith(prefix)) return;
